a244aba311fcad001bccee6b559ee5c56448b964
Pionxzh <spigbbbbb@gmail.com>
2023-07-24 21:34:37 +0800

    chore: config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,34 +1,12 @@
 module.exports = {
     root: true,
     extends: [
-        '@pionxzh/eslint-config-react',
+        '@pionxzh/eslint-config-ts',
     ],
     rules: {
         'no-console': 'off',
         'no-alert': 'off',
-        'react/prop-types': 'off',
-        'react/no-unknown-property': ['error', {
-            ignore: [
-                'stroke-width',
-                'stroke-linecap',
-                'stroke-linejoin',
-            ],
-        }],
         '@typescript-eslint/ban-ts-comment': 'off',
         '@typescript-eslint/no-unused-vars': 'off',
     },
-    overrides: [
-        {
-            files: ['template.html'],
-            rules: {
-                'no-undef': 'off',
-            },
-        },
-        {
-            files: ['**/*.md/*.*'],
-            rules: {
-                'jsonc/indent': ['error', 4],
-            },
-        },
-    ],
 }
