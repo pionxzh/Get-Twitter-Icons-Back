1b59b9ff4314407c3e7390a14ddc20bfd8a194a4
Pionxzh <spigbbbbb@gmail.com>
2023-07-25 01:51:44 +0800

    feat: replace mobile nav icon
    closes #1

--- dist/twitter.user.js
+++ dist/twitter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Get Twitter Icons Back
 // @namespace    Pionxzh
-// @version      1.1.1
+// @version      1.2.0
 // @author       Pionxzh
 // @description  Brings back the blue bird icon on Twitter. No more 𝕏.
 // @license      MIT
@@ -123,7 +123,7 @@
     }
   }
   main();
-  const twitterIconSvg = '<svg viewBox="0 0 24 24" aria-hidden="true" class="r-k200y r-1cvl2hr r-4qtqp9 r-yyyyoo r-eu3ka r-dnmrzs r-bnwqim r-1plcrui r-lrvibr old-twitter-icon"><g><path d="M23.643 4.937c-.835.37-1.732.62-2.675.733.962-.576 1.7-1.49 2.048-2.578-.9.534-1.897.922-2.958 1.13-.85-.904-2.06-1.47-3.4-1.47-2.572 0-4.658 2.086-4.658 4.66 0 .364.042.718.12 1.06-3.873-.195-7.304-2.05-9.602-4.868-.4.69-.63 1.49-.63 2.342 0 1.616.823 3.043 2.072 3.878-.764-.025-1.482-.234-2.11-.583v.06c0 2.257 1.605 4.14 3.737 4.568-.392.106-.803.162-1.227.162-.3 0-.593-.028-.877-.082.593 1.85 2.313 3.198 4.352 3.234-1.595 1.25-3.604 1.995-5.786 1.995-.376 0-.747-.022-1.112-.065 2.062 1.323 4.51 2.093 7.14 2.093 8.57 0 13.255-7.098 13.255-13.254 0-.2-.005-.402-.014-.602.91-.658 1.7-1.477 2.323-2.41z"></path></g></svg>';
+  const twitterIconSvg = '<svg data-replaced="true" viewBox="0 0 24 24" aria-hidden="true" class="r-k200y r-1cvl2hr r-4qtqp9 r-yyyyoo r-eu3ka r-dnmrzs r-bnwqim r-1plcrui r-lrvibr old-twitter-icon"><g><path d="M23.643 4.937c-.835.37-1.732.62-2.675.733.962-.576 1.7-1.49 2.048-2.578-.9.534-1.897.922-2.958 1.13-.85-.904-2.06-1.47-3.4-1.47-2.572 0-4.658 2.086-4.658 4.66 0 .364.042.718.12 1.06-3.873-.195-7.304-2.05-9.602-4.868-.4.69-.63 1.49-.63 2.342 0 1.616.823 3.043 2.072 3.878-.764-.025-1.482-.234-2.11-.583v.06c0 2.257 1.605 4.14 3.737 4.568-.392.106-.803.162-1.227.162-.3 0-.593-.028-.877-.082.593 1.85 2.313 3.198 4.352 3.234-1.595 1.25-3.604 1.995-5.786 1.995-.376 0-.747-.022-1.112-.065 2.062 1.323 4.51 2.093 7.14 2.093 8.57 0 13.255-7.098 13.255-13.254 0-.2-.005-.402-.014-.602.91-.658 1.7-1.477 2.323-2.41z"></path></g></svg>';
   const linkMap = {
     "mask-icon": "https://abs.twimg.com/responsive-web/client-web/icon-svg.168b89da.svg",
     "shortcut icon": "//abs.twimg.com/favicons/twitter.2.ico",
@@ -139,6 +139,8 @@
       replaceIcon(placeHolderIconSelector);
       const headerIconSelector = "h1 a[href='/home'] svg";
       replaceIcon(headerIconSelector);
+      const mobileHeaderIconSelector = '[data-testid="TopNavBar"] svg';
+      replaceIcon(mobileHeaderIconSelector);
       replaceLinkRel();
       replaceMetaName();
     });
@@ -174,8 +176,9 @@
   }
   function replaceIcon(selector) {
     sentinel.on(selector, (svg) => {
-      svg.innerHTML = twitterIconSvg;
-      sentinel.off(selector);
+      if (svg.getAttribute("data-replaced") !== "true") {
+        svg.innerHTML = twitterIconSvg;
+      }
     });
   }
   function replaceLinkRel() {
